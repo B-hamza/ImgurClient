cf65db0a16f25b826f616440210a2f56f2c51d70
B Hamza <hamza.bbahassou@gmail.com>
2016-01-17 15:45:46 +0100

    router training

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
-var Router = ReactRouter.Router; // the actual Router to route the pages
-var Route = ReactRouter.Route;// configure the Router
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      {this.props.children}
-    </h1>
-  }
-});
-
-var Child1 = React.createClass({
-  render:function() {
-    return <h1>
-      i'm child1
-      {this.props.children}
-    </h1>
-  }
-});
-
-var Child2 = React.createClass({
-  render:function() {
-    return <h1>i'm child2 (the other child : D )</h1>
-  }
-});
+var Routes = require('./routes');
 
 
-ReactDOM.render(routes, document.querySelector('.container'));
+
+ReactDOM.render(Routes, document.querySelector('.container'));
